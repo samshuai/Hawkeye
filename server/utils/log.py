8e30220c59d9f1e3b2669831342582af7ccd0139
Shuai <zengshuai@zhuozhuo.io>
2020-10-29 13:47:35 +0800

    change log patch

--- server/utils/log.py
+++ server/utils/log.py
@@ -3,5 +3,5 @@ import os
 import syslog
 
 FORMAT = '%(asctime)s [%(name)s] [%(levelname)s] [%(pathname)s:%(lineno)s %(funcName)s] %(message)s'
-logging.basicConfig(filename='Hawkeye.log', level=logging.INFO, format=FORMAT)
+logging.basicConfig(filename='/Hawkeye/Hawkeye.log', level=logging.INFO, format=FORMAT)
 logger = logging.getLogger()
